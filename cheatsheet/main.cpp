928796dd2a5134ca7c09ad6f112503eb6af3b120
kNakajima <kamihate1012@gmail.com>
2018-08-25 18:36:58 +0900

    チートシート

---
+++ cheatsheet/main.cpp
@@ -0,0 +1,24 @@
+//競技プログラミングチートシート(まだまだ道半ば...)
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<algorithm>
+#include <string>
+#include <vector>
+#include <list>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+using namespace std;
+
+#define INF (1e9)
+#define REP(i,n) for (int i = 0; i < n; i++)
+
+int main() {
+
+
+  return 0;
+}
