45bcda8c63a8f0be6dcccc8fe07a4787cfc98bea
kNakajima <kamihate1012@gmail.com>
2018-08-26 21:32:49 +0900

    更新

--- cheatsheet/main.cpp
+++ cheatsheet/main.cpp
@@ -1,9 +1,9 @@
 //競技プログラミングチートシート(まだまだ道半ば...)
-#include<iostream>
-#include<cstdio>
-#include<cstdlib>
-#include<cstring>
-#include<algorithm>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
 #include <string>
 #include <vector>
 #include <list>
@@ -15,7 +15,9 @@
 using namespace std;
 
 #define INF (1e9)
-#define REP(i,n) for (int i = 0; i < n; i++)
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REP(j, n) for (int j = 0; j < n; j++)
+#define REP(k, n) for (int k = 0; k < n; k++)
 
 int main() {
 
