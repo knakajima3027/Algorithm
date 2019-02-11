30a4c4287543abc2eb0e4828f440a27ada724235
kNakajima <kamihate1012@gmail.com>
2019-02-11 17:19:39 +0900

    1_5_B

---
+++ AOJ/ITP/1_5_B.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#define REP(i,n) for (int i = 0; i < n; i++)
+using namespace std;
+ 
+int main() {
+    int W, H, R[1000][1000], count = 0;
+    while (true) { 
+        cin >> H >> W;
+        if (H == 0 && W == 0) break;
+        R[count][0] = W;
+        R[count][1] = H;
+        count++;
+    }
+     
+    REP(i,count) {
+        for (int j = 0; j < R[i][1]; j++) {
+            for ( int k = 0; k < R[i][0]; k++) {
+                if (j == 0 || k == 0 || j == R[i][1] - 1|| k == R[i][0] - 1) cout << '#';
+                else cout << '.';
+            }
+            cout <<endl;
+        }
+        cout << endl;
+    }
+}
